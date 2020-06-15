485ef0ae2a62a3b195974a629d31680f7c506ed6
Bobby Neal <rsneal01@gmail.com>
2020-06-15 18:29:45 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,16 +2,20 @@ class MP3Importer
   
   attr_accessor :path
   
-  @@all = []
+  
   
   def initialize(path)
     @path = path
-    @@all << self
   end
   
   def files
     Dir.each_child(path).map {|file| file}
-    
+  end
+  
+  def import
+    files.each do |file|
+      Song.new_by_filename(file)
+    end
   end
 
 end
