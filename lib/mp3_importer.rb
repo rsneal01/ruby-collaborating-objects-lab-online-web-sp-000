f5482edbf635795122a50375017b3753b0161251
Bobby Neal <rsneal01@gmail.com>
2020-06-15 17:55:42 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,12 +9,6 @@ class MP3Importer
     @@all << self
   end
   
-  def import(list_of_filenames)
-    list_of_filenames.each do |filename|
-      Song.new_by_filename(filename)
-    end
-  end
-  
   def files
     @@all
   end
